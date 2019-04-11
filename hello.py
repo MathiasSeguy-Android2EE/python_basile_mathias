59768982856d0888b8fc87894487e903c21e9298
MathiasSeguy-Android2EE <mathias.seguy@sylpheo.com>
2019-04-11 18:38:15 +0200

    This is the initial commit

---
+++ hello.py
@@ -0,0 +1,25 @@
+#defini les variables
+photo="paris"
+string="Je suis une chaine de caractères"
+entier=1#je suis un chiffre
+#defini les foonction
+def DisCoucou(qui):
+    print( "dis coucou "+qui)
+
+def division(qui,quoi):
+    print('valeur de '+str(qui)+' divisé par '+str(quoi)+'='+str(qui/quoi))
+#execute l'application
+print("hello basile")
+print("hello papa")
+print (photo)
+print ("photo")
+print(string+"6")
+print(entier+6)
+DisCoucou("")
+DisCoucou("basile")
+DisCoucou("papa")
+division(789475687,54897)
+division(9999999999999999999999999999999999999999999999999999999999999999999999999999999999,54897)
+division(16,8)
+division(3600541,4)
+
---
+++ hello_papa.py
@@ -0,0 +1,27 @@
+print ("hello basile");
+print ("hello papa")
+
+#Definir une variable
+mathias="hello papa mathias"
+
+print (mathias)
+#Definir une autre variable
+basile="basile corto"
+#concatener deux chaines de caracteres
+print ("hello "+basile)
+#definir une fonction
+def disBonjour(qui):
+    print ("bonjour "+qui)
+    
+#utiliser
+disBonjour("patate")
+disBonjour("toto")
+
+#defini les variables
+photo="paris"
+string="Je suis une chaine de caractères"
+entier=1#je suis un chiffre
+#defini les foonction
+def DisCoucou(qui):
+    print( "dis coucou "+qui)
+
